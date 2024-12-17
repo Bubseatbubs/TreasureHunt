6ab20f2947cb4b76906e4874fa06fc9add7ce8f0
Bubseatbubs <bubseatbubs@gmail.com>
2024-12-16 19:09:23 -0800

    Update UI to look nicer

--- Scripts/UI/PlayerStats.cs
+++ Scripts/UI/PlayerStats.cs
@@ -9,13 +9,19 @@ public class PlayerStats : MonoBehaviour
     TextMeshProUGUI weightText;
 
     [SerializeField]
-    TextMeshProUGUI inventoryCountText;
-
-    [SerializeField]
     TextMeshProUGUI itemValueText;
 
     [SerializeField]
     TextMeshProUGUI balanceText;
+
+    [SerializeField]
+    Transform inventory;
+
+    [SerializeField]
+    GameObject itemPrefab;
+
+    const float X_OFFSET = 20;
+    const float Y_OFFSET = -100;
 
     void Start()
     {
@@ -24,14 +30,32 @@ public class PlayerStats : MonoBehaviour
 
     public void UpdateStats(double weight, double itemValue, int inventoryCount, double balance)
     {
-        weightText.text = $"Weight: {weight} LB";
-        itemValueText.text = $"Value Of Items: ${itemValue}";
-        inventoryCountText.text = $"Items in Inventory: {inventoryCount}";
-        balanceText.text = $"Balance: ${balance}";
+        weightText.text = $"{weight} LB";
+        itemValueText.text = $"${itemValue}";
+        balanceText.text = $"${balance}";
+        UpdateInventoryCount(inventoryCount);
+    }
+
+    public void UpdateInventoryCount(int count)
+    {
+        // Clear all coins
+        foreach (Transform item in inventory)
+        {
+            Destroy(item.gameObject);
+        }
+
+        // Put new coins
+        for (int i = 0; i < count; i++)
+        {
+            GameObject coin = Instantiate(itemPrefab, inventory);
+            Vector2 currentOffset = new Vector2(i * X_OFFSET, Y_OFFSET);
+            coin.transform.position = (Vector2) coin.transform.position + currentOffset;
+        }
     }
 
     public void ResetStats()
     {
         UpdateStats(0.0, 0, 0, 0);
+        UpdateInventoryCount(0);
     }
 }
